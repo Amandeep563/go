0a801d0a520616973b6b505162c4b7bf0afd6a1b
Aman <amandeepbollampalli@gmail.com>
2025-10-01 12:36:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello this my first golang code");
+	fmt.Println("Hello this my first golang code")
 	fmt.Println("hey second line")
 	fmt.Println("hmmm")
 	fmt.Println("hello world")
+	fmt.Println("dddd")
 }
